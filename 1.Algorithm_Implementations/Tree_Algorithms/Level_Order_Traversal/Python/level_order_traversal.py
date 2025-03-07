46b46df594c842aed510d59cdaa75947deac622b
Alex Nunez <josenunez1692@gmail.com>
2025-03-07 12:14:04 -0500

    Add level order traversal implementation in Python and moved Binary_Tree_Traversal/ under Tree_Algorithms

---
+++ 1.Algorithm_Implementations/Tree_Algorithms/Level_Order_Traversal/Python/level_order_traversal.py
@@ -0,0 +1,51 @@
+from queue import Queue
+
+# Iterative Implementation
+def level_order_traversal_iterative(tree):
+    array = []
+
+    if tree is None:
+        return array
+
+    queue = Queue()
+    queue.put(tree)
+
+    while not queue.empty():
+        current = queue.get()
+        array.append(current.value)
+
+        if current.left:
+            queue.put(current.left)
+        if current.right:
+            queue.put(current.right)
+
+    return array
+
+
+# recursive implementation
+def level_order_traversal_recursive(tree):
+    array = []
+
+    if tree is None:
+        return array
+
+    queue = Queue()
+    queue.put(tree)
+    return level_order_traversal_recursive_helper(array, queue)
+
+
+def level_order_traversal_recursive_helper(array, queue):
+    # Base Case
+    if queue.empty():
+        return array
+
+    current = queue.get()
+    array.append(current.value)
+
+    if current.left:
+        queue.put(current.left)
+    if current.right:
+            queue.put(current.right)
+
+    return level_order_traversal_recursive_helper(array, queue)
+    
