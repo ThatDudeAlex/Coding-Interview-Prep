852aace8de807ba370e673485de962f01515d598
Alex Nunez <josenunez1692@gmail.com>
2025-03-11 02:25:55 -0400

    Cleanup on chp 1 question 3 of cracking the coding interview

--- 3.Platform_Solutions/Cracking-the-Coding-Interview/Ch 01. Arrays and Strings/03_URLify/Python/urlify.py
+++ 3.Platform_Solutions/Cracking-the-Coding-Interview/Ch 01. Arrays and Strings/03_URLify/Python/urlify.py
@@ -1,60 +1,23 @@
-from collections import deque
 
-
-# def replace_spaces(input_str, actual_len):
-# REPLACEMENT = "%20"
-# output = ""
-# curr_str = ""
-
-# for i in range(actual_len):
-#     if input_str[i] != " ":
-#         curr_str += input_str[i]
-#     elif curr_str:
-#         output += curr_str
-#         output += REPLACEMENT
-#         curr_str = ""
-#     else:
-#         output += REPLACEMENT
-
-# if curr_str:
-#     output += curr_str
-
-# return output
+# o(n) time complexity
 def replace_spaces(input_str, actual_len):
-    queue = build_queue(input_str, actual_len)
-
-    str_idx = 0
-
-    while str_idx < len(input_str):
-        curr_str = queue.popleft()
-
-        for char in curr_str:
-            input_str[str_idx] = char
-            str_idx += 1
-
-    return input_str
-
-
-def build_queue(input_str, length):
     REPLACEMENT = "%20"
-    queue = deque()
+    output = ""
     curr_str = ""
 
-    for i in range(length):
+    for i in range(actual_len):
         if input_str[i] != " ":
             curr_str += input_str[i]
         elif curr_str:
-            queue.append(curr_str)
-            queue.append(REPLACEMENT)
+            output += curr_str
+            output += REPLACEMENT
             curr_str = ""
         else:
-            queue.append(REPLACEMENT)
+            output += REPLACEMENT
 
     if curr_str:
-        queue.append(curr_str)
+        output += curr_str
 
-    return queue
+    return output
 
 
-output = replace_spaces("Mr John Smith    ", 13)
-print(output)
